550bb716bd3ef19d31b319e9f52fc63510e8e233
gsalazar2023 <griel.salazart79@gmail.com>
2026-01-29 17:16:49 -0300

    Enemy Navmesh AI Patrol and Chase

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,61 +1,154 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.TextCore.Text;
 
 public class EnemyController : MonoBehaviour
 {
+
     //Components
     public GameObject tpPointer;
-    public CardSpawner spawner;
-    public GameObject objCard;
+    public NavMeshAgent agent;
+    public Transform playerPos;
     [SerializeField] public ParticleSystem tpCloudE;
 
     //Variables
     [SerializeField] public int enemyLife = 3;
-    [SerializeField] public Transform raycast;
-    [SerializeField] public LayerMask groundLayer;
-    public float animDelay = 0.5f;
-    public float rayDist = 50f;
-    public Vector3 finalPos;
+    [SerializeField] public LayerMask groundLayer, playerLayer;
+    public float rayDist = 5f;
+
+    bool playerVisible;
+    bool playerReachable;
+
+    float lastSeen;
+    float seenTimer = 2.5f;
+
+    //Patrol
+    [SerializeField] public Transform waypoints;
+    int waypointIndex;
+    float waitTime = 1f;
+    float timer = 0f;
 
     //States
-    public float sightRange;
-    public bool playerInSight, playerInAttack; //Range
+    public float sightRange, attackRange;
+    public bool playerInRange, attackInRange;
+
+
+
+    public float animDelay = 0.5f;
    
     void Start()
     {
         tpPointer.SetActive(false);
-        spawner = GetComponent<CardSpawner>(); 
+        agent = GetComponent<NavMeshAgent>();
+        playerPos = GameObject.Find("Player").transform;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        EnemyRaycast();
-    }
+        
+        playerVisible = false;
+        playerInRange = Physics.CheckSphere(transform.position, sightRange, playerLayer);
+        attackInRange = Physics.CheckSphere(transform.position, attackRange, playerLayer);
 
-    void EnemyRaycast()
-    {
-        Vector3 origin = raycast.position;//punto de origen de ray cast
-        Vector3 downDir = raycast.transform.TransformDirection(Vector3.down * rayDist);//direccion de raycast /mirando hacia abajo
-        //Vector3 forwardDir = transform.TransformDirection(Vector3.forward * rayDist);//direccion de raycast /frente
-
-        //Ray ray = new Ray(origin, forwardDir);
-        //finalPos = ray.GetPoint(rayDist);
-
-        if (Physics.Raycast(origin, downDir, out RaycastHit hit, rayDist, groundLayer))
+        if (playerInRange)//si detecta jugador = jugador es visible
         {
-            //Debug.Log("ENEMY : Looking at Ground");
+            Vector3 dir = (playerPos.transform.position - transform.position).normalized;
+            if (Physics.Raycast(transform.position + Vector3.up, dir, out RaycastHit hit, sightRange))
+            {
+                if (hit.collider.CompareTag("Player"))
+                {
+                    Debug.Log("Player is within sight");
+                    playerVisible = true;
+                }
+                else
+                {
+                    Debug.Log("Can't see player");
+                }
+            }
+        }
 
-            //Debug.DrawRay(origin, downDir * rayDist, Color.red);
-            finalPos = hit.point;//pos final enem = raycast
+        if (playerVisible) //si jugador es visible = jugador se puede alcanzar
+        {
+            NavMeshPath path = new NavMeshPath();
+            agent.CalculatePath(playerPos.transform.position, path);//calcula camino hacia pos de jugador
 
+            if (path.status == NavMeshPathStatus.PathComplete) 
+            {
+                Debug.Log("Player can be reached");
+                playerReachable = true;
+            }
+            else
+            {
+                Debug.Log("Can't reach player");
+            }
+                
+        }
 
+        if (playerVisible && playerReachable && !attackInRange) //si jugador es visible y se puede alcanzar
+        {
+            lastSeen = 0f;
+            Chasing();//empezar a perseguir
         }
         else
         {
-            Debug.Log("ENEMY : NOT Looking at ground");
+            lastSeen += Time.deltaTime;
+            if (lastSeen >= seenTimer)
+            {
+                Patroling();
+            }
+        }
+
+        if (!playerInRange && !attackInRange) Patroling();
+        //if (playerInRange && !attackInRange) Chasing();
+        
+
+    }
+
+
+    void Attacking()
+    {
+        Debug.Log("Player in range");
+    }
+
+    void Chasing()
+    {
+        agent.SetDestination(playerPos.position);
+    }
+
+    void Patroling()
+    {
+        if (agent.remainingDistance <= 2f)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= waitTime)
+            {
+                waypointIndex++;
+                timer = 0f;
+                if (waypointIndex >= waypoints.childCount)
+                {
+                    waypointIndex = 0;
+
+                }
+
+                Debug.Log("ENEMY HEADING TOWARDS " + waypoints.GetChild(waypointIndex));
+                agent.SetDestination(waypoints.GetChild(waypointIndex).position);
+            }
+
         }
     }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+
 
     public void TakeDamage()
     {
@@ -74,15 +167,7 @@ public class EnemyController : MonoBehaviour
         
     }
 
-    void Attacking()
-    {
-        Debug.Log("Hitting Player");
-    }
-
-    void Chasing()
-    {
-        Debug.Log("Chasing Player");
-    }
+   
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -92,12 +177,10 @@ public class EnemyController : MonoBehaviour
 
 
         }
-
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && attackInRange)
         {
             Attacking();
         }
-
     }
 
 }
